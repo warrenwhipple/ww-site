9bd01167cce96703249620615fc3cf0d8d787c23
Warren Whipple <modalrealist@gmail.com>
2018-06-27 09:32:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ww-site
+
+Warren Whipple website.
+
+Built with [Gatsby](https://www.gatsbyjs.org/).
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <div>Warren Whipple</div>
+    <div>
+      <a href="mailto:w@warrenwhipple.com">w@warrenwhipple.com</a>
+    </div>
+  </div>
+);
