d1cf1a403c906ccf0dd184bbd424f622fbe3eadb
Warren Whipple <modalrealist@gmail.com>
2018-06-27 13:01:27 -0400

    Add resume html

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,58 @@ import React from "react";
 export default () => (
   <div>
     <h1>Warren Whipple</h1>
+    <div>Web Developer + Social/Clinical Research Specialist</div>
     <div>
       <a href="mailto:w@warrenwhipple.com">w@warrenwhipple.com</a>
     </div>
+    <h2>Experience</h2>
+    <h3>
+      Research Specialist + Web Developer + Program Manager,<br />UNC Department
+      of Social Medicine
+    </h3>
+    <h4>2008 – 2018</h4>
+    <p>
+      Researcher + web developer, UNC Department Social Medicine. Researcher +
+      web developer, UNC Center for Bioethics. Program manager, Clinical Ethics
+      Research Service, NC Translational and Clinical Sciences Institute.
+      Researcher, UNC Hospital Ethics Committee.
+    </p>
+    <h2>Skills</h2>
+    <h3>WordPress</h3>
+    <p>
+      Site creation and management on UNC WordPress servers. Server deployment
+      on private cloud hosting. Site migration. Custom CSS styling. JS
+      integration. PHP plugin coding. Custom type and template development with
+      WP Toolset.
+    </p>
+    <h3>JavaScript</h3>
+    <p>ES6. Node. Babel. Webpack. React. Gatsby.</p>
+    <h3>Analytics + dev tools</h3>
+    <p>
+      Google Analytics. Splunk. Git. GitHub. BitBucket. VS Code. XCode. Sublime.
+      Eclipse.
+    </p>
+    <h3>Other dev stacks + languages</h3>
+    <p>
+      PHP + Oracle DB. Static HTML + CSS. SCSS. Python. C. Objective C. Swift.
+      Metal. CloudKit.
+    </p>
+    <h3>Office + research + design tools</h3>
+    <p>
+      Asana. Trello. Slack. Word. Excel. PowerPoint. Access. EndNote. Refworks.
+      Zotero. Stata. Acrobat. Photoshop. Illustrator. InDesign. Inkscape.
+      Blender.
+    </p>
+    <h2>Education</h2>
+    <h3>Computer Science Courses, University of North Carolina Chapel Hill</h3>
+    <h4>2015 – 2016</h4>
+    <p>
+      Discrete Structures. Algorithms and Analysis. Differential Equations 1 +
+      Computational Lab. Under the UNC employee tuition waiver program.
+    </p>
+    <h3>
+      BA Philosophy w Honors + BS Biology, University of Missouri Columbia
+    </h3>
+    <h4>1998 – 2003</h4>
   </div>
 );
